ac555c2c6eeff6a54b06cc041b91f73306520b1e
Egor Goroshkin <49096456+Mkolba@users.noreply.github.com>
2024-10-19 14:21:02 +0500

    fix

--- src/view/window/scroll-window.js
+++ src/view/window/scroll-window.js
@@ -1,7 +1,11 @@
 // @flow
 import { type Position } from 'css-box-model';
+import bridge from "@vkontakte/vk-bridge";
 
 // Not guarenteed to scroll by the entire amount
 export default (change: Position): void => {
+  bridge.send('VKWebAppScroll', {top: change.y}).then(data => {
+    console.log(data)
+  })
   window.scrollBy(change.x, change.y);
 };
